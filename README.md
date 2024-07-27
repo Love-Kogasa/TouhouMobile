7615c33243e05aa75718aae7d78b99beb5ad8d6d
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-07-27 15:20:35 +0800

    Root dir

---
+++ README.md
@@ -0,0 +1,13 @@
+# TouhouMobile
+<ul>
+TouhouMobile 简称 THM<br>
+是一个为移动端(特别是安卓)提供的
+<ul>
+资源站+工具站
+</ul>
+用于收录，采集车万的手机资源<br>
+并提供一些工具w ( 以后会越来越多的啦 )<br>
+这样就可以省去寻找车万手机资源的时间啦
+</ul>
+<hr>
+<div align="right"><a href="#">启动！w</a></div>
---
+++ global.js
@@ -0,0 +1,46 @@
+var touhouMobile = {
+    stg : {
+        "东方夏夜祭" : {
+            url : "http://m.xz7.com/downinfo/596997.html",
+            info : "一款由电脑移植的车万stg游戏<br>从手感和体验来说都相当不错w<br>不太适合新手玩<br>",
+            star : 5
+        },
+        "东方恋觉想" : {
+            url : "https://m.newasp.com/android/654650.html",
+            info : "一个手机独占的车万STG作<br>用的来说很不错<br>手感差一些<br>",
+            star : 4
+        },
+        "东方千夜帖" : {
+            url : "https://m.newasp.com/android/702908.html",
+            info : "一款相当好的车万stg手游<br>收录了大部分正作的角色，符卡，自机<br>推荐新手试一试<br>",
+            star : 5
+        },
+        "东方魔导录" : {
+            url : "https://www.168you.com/game/12507.html",
+            info : "手感很不错，UI也很不错，很流畅<br>支持莎莎，灵梦，早苗，妖梦，咲夜五中自机<br>类似于上者，比上者功能多一些，ui更好看<br>符卡没有上者多<br>",
+            star : 5
+        },
+        "东方幻灵梦" : {
+            url : "http://m.downcc.com/d/56720",
+            info : "这作真的超烂，操作有点小问题不重要<br>对新手来说很难，而且素材也很烂，直接用的黄昏绘<br> ---来自LoveKogasa的吐槽<br>",
+            star : 2
+        },
+        "BakaSTG" : {
+            url : "http://m.downcc.com/d/284948",
+            info : "一个经典风的车万STG手游<br>手感不错，风格也很不错w<br>https://minagi.me/archives/1552",
+            star : 4
+        },
+        "弹幕游戏" : {
+            url : "http://m.downcc.com/d/284934",
+            info : "类似于上者，也是一个经典风的车万STG手游<br>总体来说还是挺不错的",
+            star : 4
+        }
+    },
+    其他 : {
+        "⑨的完美算数教室(游戏)" : {
+            url : "#",
+            info : "BAKA BAKA",
+            star : 5
+        }
+    }
+}
