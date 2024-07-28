c5c907307cbfe94bb098b584657e5fd91751ed23
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-07-28 09:50:13 +0800

    Upload list.js

---
+++ script/tools/list.js
@@ -0,0 +1,14 @@
+var listele = document.getElementById( "tools" ),
+  list = [ 
+    [ "PyOnline", "在线运行你的Python脚本w<br>支持分享哦✓", "./py.html" ]
+  ]
+listele.innerHTML = (function(){
+    var html = ""
+    for( let tool of list ){
+        html += `<tool>
+          <name><a href="${tool[2]}" style="color : #FFF;">${tool[0]}</a></name>
+          <info>${tool[1]}</info>
+        </tool>`
+    }
+    return html
+})()
