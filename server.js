8385e95ff395f06107cf1d9a124cc2d752f4df7b
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-07-29 09:17:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 server = {
     version : 0.1,
-    info : "TouhouMobile AndroidAPP 的第一个版本<br>现在你可以更快的访问THM了"
+    info : "TouhouMobile AndroidAPP 的第一个版本\n现在你可以更快的访问THM了"
 }
 if( client.version == server.version/*版本号*/ ){
     alert( "已是最新版本w" )
 } else {
-    alert( `THM${server.version}更新啦<br>更新内容 : ${server.info}` )
+    alert( `THM${server.version}更新啦\n更新内容 : ${server.info}` )
 }
